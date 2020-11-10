023fdc68b3259d3818bbcc02344197cea626a53d
Всеволод html <63463168+htmlcssphpjs@users.noreply.github.com>
2020-11-10 18:40:52 +0300

    #lib$

---
+++ lib.js
@@ -0,0 +1,67 @@
+globalThis.$ = {
+	find: function (dom) {
+	  return document.querySelector(dom);
+	},
+  rem: function (dom) {
+    document.querySelector(dom).remove();
+  },
+  popup: function (dom) {
+    let d = document.querySelector(dom);
+    if (d.style.display == "none") {
+        d.style.display = "block";
+    } else if (d.style.display == "block") {
+        d.style.display = "none";
+    } else {
+        console.warn('Не заданно display: block или none');
+        d.style.display = "block";
+    }
+  },
+  hide: function (dom) {
+    document.querySelector(dom).style.display = "none";
+  },
+  show: function (dom) {
+    let d = document.querySelector(dom);
+    if (d.style.display == "none") {
+      d.style.display = "block";
+    } else {
+      console.warn('Как я тебе это отображу? Поставь display: none; хоть!!!');
+    }
+  },
+  log: function (text) {
+	  console.log('%c%s', 'color: black; font: 1.2rem/1 Tahoma;', text);
+  },
+  info: function (dom) {
+	  console.log('%O', document.querySelector(dom));
+  },
+  logimg: function (i,s,c) {
+	  const r = new FileReader();
+        if(s==undefined){s=100};
+	  r.addEventListener('load', function () {
+	    const o ='background: url(\'' + r.result + '\') left top no-repeat; font-size: '+s+'px; background-size: contain; background-color:'+c;
+	    console.log('%c     ',o);
+	  },false);
+	  fetch(i)
+	    .then(r=>r.blob())
+	    .then(b=>{
+	  	if(b.type.indexOf('image')===0){
+	  	  if(b.size>8192&& navigator.userAgent.indexOf('Firefox')>0){
+	  		throw new Error('Изображение СЛИШКОМ большое.');
+	  	  }
+	  	  return b;
+	  	}else{
+	  	  throw new Error('Хм... Неправильный адрес.');
+	  	}
+	    })
+	    .then(i=>r.readAsDataURL(i))
+	    .catch(e=>console.warn(e.message));
+	},
+  click: function (dom) {
+    document.querySelector(dom).click();
+  },
+  text: function (dom, text) {
+    document.querySelector(dom).textContent = text;
+  },
+  add: function (dom, cl) {
+    document.querySelector(dom).classList.add(cl);
+  }
+};
